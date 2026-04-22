d16169dda60d787d0774360dce62f20b2d407f5e
Frederic Bourouliou <bourouliou@gmail.com>
2026-04-22 07:27:08 +0200

    feat: Refactor l'écran d'inscription avec des champs de saisie et une gestion améliorée des mots de passe

--- holbegram/lib/screens/signup_screen.dart
+++ holbegram/lib/screens/signup_screen.dart
@@ -1,18 +1,160 @@
 import 'package:flutter/material.dart';
 
-class SignUpScreen extends StatefulWidget {
-  const SignUpScreen({super.key});
+import '../widgets/text_field.dart';
+import 'login_screen.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
 
   @override
-  State<SignUpScreen> createState() => _SignUpScreenState();
+  State<SignUp> createState() => _SignUpState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
+class _SignUpState extends State<SignUp> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController passwordConfirmController =
+      TextEditingController();
+  late bool _passwordVisible;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = true;
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    usernameController.dispose();
+    passwordController.dispose();
+    passwordConfirmController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Center(child: Text('Sign Up')),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 28),
+              const Text(
+                'Holbegram',
+                style: TextStyle(fontFamily: 'Billabong', fontSize: 50),
+              ),
+              Image.asset(
+                'assets/images/logo.webp',
+                width: 80,
+                height: 60,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Text(
+                  'Sign up to see photos and videos from your friends.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.grey, fontSize: 15),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 28),
+                    TextFieldInput(
+                      controller: emailController,
+                      ispassword: false,
+                      hintText: 'Email',
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 24),
+                    TextFieldInput(
+                      controller: usernameController,
+                      ispassword: false,
+                      hintText: 'Username',
+                      keyboardType: TextInputType.text,
+                    ),
+                    const SizedBox(height: 24),
+                    TextFieldInput(
+                      controller: passwordController,
+                      ispassword: !_passwordVisible,
+                      hintText: 'Password',
+                      keyboardType: TextInputType.visiblePassword,
+                      suffixIcon: IconButton(
+                        alignment: Alignment.bottomLeft,
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    TextFieldInput(
+                      controller: passwordConfirmController,
+                      ispassword: !_passwordVisible,
+                      hintText: 'Confirm Password',
+                      keyboardType: TextInputType.visiblePassword,
+                    ),
+                    const SizedBox(height: 28),
+                    SizedBox(
+                      height: 48,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(
+                            const Color.fromARGB(218, 226, 37, 24),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Sign up',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    const Divider(thickness: 2),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Already have an account?'),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => const LoginScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              'Log in',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(218, 226, 37, 24),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
